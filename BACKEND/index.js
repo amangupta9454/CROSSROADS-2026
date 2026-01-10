5c0415f45f1be2681f51a010448e3d21e6f1651e
CODE NOVA <codenova31@gmail.com>
2026-01-10 18:09:18 +0530

    backend hosted

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -11,10 +11,13 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/registrations', registrationRoutes);
-
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 const PORT = process.env.PORT || 5000;
 console.log("Cloudinary config loaded:");
   console.log("CLOUD_NAME:", process.env.CLOUDINARY_CLOUD_NAME);
   console.log("API_KEY:", process.env.CLOUDINARY_API_KEY ? "exists" : "MISSING");
   console.log("API_SECRET:", process.env.CLOUDINARY_API_SECRET ? "exists" : "MISSING");
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
