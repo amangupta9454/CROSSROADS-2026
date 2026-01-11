4848b62fb5fa5af4cba1b49dea2693d50ac64bfc
CODE NOVA <codenova31@gmail.com>
2026-01-11 15:12:07 +0530

    index

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -7,7 +7,7 @@ const registrationRoutes = require('./routes/registrationRoutes');
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors("*"));
 app.use(express.json());
 
 app.use('/api/registrations', registrationRoutes);
